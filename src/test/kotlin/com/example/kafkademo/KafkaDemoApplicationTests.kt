c0826a8311b5529e94c84707c3a1421f27c1f4b8
Vedant Nevetia <nevetia.vedant@gmail.com>
2020-11-02 14:56:17 -0600

    add unit tests

--- src/test/kotlin/com/example/kafkademo/KafkaDemoApplicationTests.kt
+++ src/test/kotlin/com/example/kafkademo/KafkaDemoApplicationTests.kt
@@ -13,26 +13,40 @@ class KafkaDemoApplicationTests {
 
 	@Test
 	fun `test getTotal returns the right value pre-discount`() {
-
+		val orderList = listOf("Apple", "Apple", "Orange", "Apple")
+		val order = Order(orderList)
+		Assertions.assertEquals(order.getOrderTotal(), 2.05)
 	}
 
 	@Test
 	fun `test getTotal returns the right value post-discount`() {
-
+		val orderList = listOf("Apple", "Apple", "Orange", "Apple")
+		val order = Order(orderList)
+		order.applyDiscount()
+		Assertions.assertEquals(order.getOrderTotal(), 1.45)
 	}
 
 	@Test
 	fun `test updateStock sets the stock to the right number`() {
-
+		val orderList = listOf("Apple", "Apple", "Orange", "Apple")
+		val order = Order(orderList)
+		Stock.updateStock(order)
+		Assertions.assertEquals(Stock.getStock(), mapOf<String, Int>("Apple" to 7, "Orange" to 9))
 	}
 
 	@Test
 	fun `test getOutOfStockItems`() {
-
+		val orderList = listOf("Apple", "Apple", "Apple", "Apple", "Apple", "Apple", "Apple", "Apple", "Apple", "Apple",
+		"Orange", "Orange", "Orange", "Orange", "Orange", "Orange", "Orange", "Orange", "Orange", "Orange", "Orange")
+		val order = Order(orderList)
+		val outOfStockItems = Stock.getOutOfStockItems(order)
+		Assertions.assertEquals(outOfStockItems, listOf("Orange"))
 	}
 
 	@Test
-	fun `test random items in order get ignored`() {
-
+	fun `test that random items in an order get ignored`() {
+		val orderList = listOf("Apple", "Orange", "Orange", "Banana", "Apple")
+		val order = Order(orderList)
+		Assertions.assertEquals(mapOf("Apple" to 2, "Orange" to 2), order.getOrder())
 	}
 }
